ed1589685e7a6a226ae3ee1055a2cb0d46b9fa0d
Narsi <narsi.myteaching@gmail.com>
2022-06-23 09:40:22 +0530

    added the bom-demo, profiles-demo and conditional-annotation-demo applications

---
+++ bill-of-materilas-demo/src/main/java/com/weshopify/retailecom/Application.java
@@ -0,0 +1,10 @@
+package com.weshopify.retailecom;
+
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("iam in Application.java main method");
+
+	}
+
+}
