fee4c3c3e17f146fdd544a15cad889c044e3e03e
Narsi <narsi.myteaching@gmail.com>
2022-06-27 09:38:24 +0530

    added the Customer Self Registration and Admin Registration

--- we-shopify-platform/src/main/java/com/weshopify/platform/features/customers/CustomerController.java
+++ we-shopify-platform/src/main/java/com/weshopify/platform/features/customers/CustomerController.java
@@ -1,7 +1,10 @@
 package com.weshopify.platform.features.customers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -16,8 +19,22 @@ public class CustomerController {
 	}
 	
 	@RequestMapping("/add-customer-view")
-	public String addCustomerViewPage() {
+	public String addCustomerViewPage(Model model) {
 		log.info("i am inn addCustomerViewPage page");
+		model.addAttribute("customerFormBean", new CustomerBean());
+		return "add-customer";
+	}
+	
+	@RequestMapping(value = "/save-customer",method = RequestMethod.POST)
+	public String addCustomer(@ModelAttribute("customerFormBean") CustomerBean customerBean) {
+		log.info("i am inn addCustomerViewPage page");
+		log.info(customerBean.toString());
+		
+		if(customerBean.getIsSelfReg() != null && Boolean.valueOf(customerBean.getIsSelfReg())) {
+			//Todo customer Self Registration
+		}else {
+			//Todo: Register the customer by the admin
+		}
 		return "add-customer";
 	}
 }
